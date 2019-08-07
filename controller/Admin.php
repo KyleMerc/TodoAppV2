a095b4ab4c67d6c9cb071fb8a759ab3c41b228ef
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-08-07 14:07:02 +0800

    Done Edit for Admin only lacks verification

--- controller/Admin.php
+++ controller/Admin.php
@@ -1,23 +1,24 @@
 <?php
 session_start();
-print_r($_POST);die;
+// print_r($_POST);die;
 require "../model/config.php";
 
-var_dump($_POST);die;
+// var_dump($_POST);die;
 if( ! isset($_SESSION)) {
     header("Location: ../index.php");
 }
 
 
-// if(isset($_POST["adminSubmit"])) {
-//     $content = htmlspecialchars($_POST["addContent"]);
-//     $user_id = $_POST["v_id"];
-//     $sql = "INSERT INTO todos(content, user_id, status) 
-//             VALUES('$content', '$user_id', 'IN PROGRESS')";
+if(isset($_POST["editUser"])) {
+    $username = $_POST['newUsername'] == $_POST['oldUsername']? $_POST['oldUsername'] : $_POST['newUsername'];
+    $password = $_POST["newPassword"] == ''? $_POST['oldPassword'] : password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
+    $user_id = $_POST['userId'];
+    $sql = "UPDATE users
+            SET username='$username', password='$password', date_updated=CURRENT_TIMESTAMP
+            WHERE user_id='$user_id'";
+            
 
-//    query($sql, $mysqli);
+   query($sql, $mysqli);
+}
 
-//    header("Location: ../views/admin/view_admin_home.php?v_id=$user_id");
-// }
-
-// header("Location: ../views/admin/view_admin_home.php");
+header("Location: ../views/admin/view_admin_home.php");
---
+++ views/admin/view_admin_edit_user.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+
+require "../../model/config.php";
+
+$user_id = $_GET['id'] ?? false;
+
+$sql = "SELECT * FROM users WHERE user_id='$user_id'";
+$result = query($sql, $mysqli);
+
+$data = mysqli_fetch_assoc($result);
+?>
+
+<?php require "../template/header.php"; ?>
+    <?php if(mysqli_num_rows($result) > 0 && $data['user_role_id'] != 1) : ?>
+        <h3>Edit User <?php echo $data["username"]; ?></h3>
+
+        <form action="../../controller/Admin.php" class="form" method="post">
+            <input type="hidden" name="oldUsername" value="<?php echo $data['username']; ?>">
+            <input type="hidden" name="oldPassword" value="<?php echo $data['password']; ?>">
+            <input type="hidden" name="userId" value="<?php echo $data['user_id']; ?>">
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="" class="font-weight-bold">Date Created</label>
+                    <input type="text" class="form-control-plaintext" value="<?php echo date('M j Y g:i A', strtotime($data["date_created"])); ?>">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="" class="font-weight-bold">Date Updated</label>
+                    <input type="text" class="form-control-plaintext" value="<?php echo date('M j Y g:i A', strtotime($data["date_updated"]));?>">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-auto">
+                    <label for="" class="font-weight-bold">Username</label>
+                    <input type="text" class="form-control" value="<?php echo $data['username']; ?>" name="newUsername">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-auto">
+                    <label for="" class="font-weight-bold">Password</label>
+                    <input type="password" class="form-control" name="newPassword">
+                </div>
+                <div class="form-group col-auto">
+                    <label for="" class="font-weight-bold">Confirm Password</label>
+                    <input type="password" class="form-control" name="confNewPassword">
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="form-group col">
+                    <a href="view_admin_home.php" class="btn btn-primary">Go back</a>
+                </div>
+                <div class="form-group col-9">
+                    <button type="submit" class="btn btn-primary" type="submit" name="editUser">Save</button>
+                </div>
+            </div>
+        </form>
+    <?php else : ?>
+        <div class="jumbotron">
+            <div class="display-3 text-danger">No user found!</div>
+        </div>
+        <a href="view_admin_home.php" class="btn btn-primary">Go back</a>
+    <?php endif; ?>
+
+    
+<?php require "../template/footer.php"; ?>
