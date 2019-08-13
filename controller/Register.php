902a40c011611dc9ca173940e2ddabc53bffd7ac
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-08-13 18:50:27 +0800

    Transferred some functions to loadFunction.php

--- controller/Register.php
+++ controller/Register.php
@@ -1,12 +1,7 @@
 <?php
 
 require "loadFunction.php";
-require_once "../vendor/autoload.php";
 
-use \PHPMailer\PHPMailer\PHPMailer;
-use \PHPMailer\PHPMailer\Exception;
-
-session_start();
 
 $conn = $mysqli;
 
@@ -20,7 +15,6 @@ function register($conn) {
         $user = htmlspecialchars($_POST["username"]);
         $pass = htmlspecialchars($_POST["password"]);
         $confPass = htmlspecialchars($_POST["confPassword"]);
-        $mail = new PHPMailer(true);
 
         if(empty($user)) {
             $_SESSION['emptyUser'] = 'emptyUser';
@@ -34,9 +28,10 @@ function register($conn) {
             die;
         }
 
-        $verify_sql = "SELECT * FROM users WHERE username='$user'";
-        $result = query($verify_sql, $conn);
-        $exist = mysqli_num_rows($result);
+        // $verify_sql = "SELECT * FROM users WHERE username='$user'";
+        // $result = query($verify_sql, $conn);
+        $exist = check_username($user);
+
 
         //encrypt password
         $encPass = password_hash($pass, PASSWORD_DEFAULT);
@@ -48,32 +43,7 @@ function register($conn) {
                             VALUES('$user', '$encPass', 2, '$firstName', '$lastName', '$email', '$vkey')";
             query($insert_sql, $conn);
             
-            
-            //----Send Mail
-                //Server settings
-                $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-                $mail->isSMTP();                                            // Set mailer to use SMTP
-                $mail->Host       = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
-                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'kyle.fst@yahoo.com';                     // SMTP username
-                $mail->Password   = 'CYK%BDcxJRm7Rwd';                               // SMTP password
-                $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
-                $mail->Port       = 465;                                    // TCP port to connect to
-            
-                //Recipients
-                $mail->setFrom('kyle.fst@yahoo.com', 'TodoApp');
-                $mail->addAddress($email, $firstName.' '.$lastName);     // Add a recipient
-                
-                //Body
-                $body = "To verify your account <br /><a href='192.168.64.2/TODOapp/views/user/view_verify.php?vkey=$vkey'>Click Here</a>";
-                
-                // Content
-                $mail->isHTML(true);                                  // Set email format to HTML
-                $mail->Subject = 'Email Verification';
-                $mail->Body    = $body;
-                // $mail->AltBody = strip_tags($body);
-            
-                $mail->send();
+            send_verification($email, $vkey);
 
             header("Location: ../views/user/thankyou.php");
             die;
--- controller/loadFunction.php
+++ controller/loadFunction.php
@@ -1,10 +1,41 @@
 <?php
 session_start();
 require_once "../model/config.php";
+require_once "../vendor/autoload.php";
+
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\Exception;
 
 //Email verification
-function send_verification() {
+function send_verification($email, $vkey) {
+     //----Send Mail
+     $mail = new PHPMailer(true);
+
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.mail.yahoo.com';                  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'kyle.fst@yahoo.com';                   // SMTP username
+    $mail->Password   = 'CYK%BDcxJRm7Rwd';                      // SMTP password
+    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('kyle.fst@yahoo.com', 'TodoApp');
+    $mail->addAddress($email);     // Add a recipient
     
+    //Body
+    $body = "To verify your account <br /><a href='192.168.64.2/TODOapp/views/user/view_verify.php?vkey=$vkey'>Click Here</a>";
+    
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Email Verification';
+    $mail->Body    = $body;
+    // $mail->AltBody = strip_tags($body);
+
+    $mail->send();
+
 }
 
 function verify_key($vkey){
@@ -12,7 +43,7 @@ function verify_key($vkey){
 
     $check = $GLOBALS['mysqli']->query($sql);
 
-    if($check->num_rows() == 1) {
+    if(mysqli_num_rows($check) == 1) {
         $update_sql = "UPDATE users SET verified = 1 WHERE vkey = '$vkey";
         $GLOBALS['mysqli']->query($update_sql);
 
@@ -34,6 +65,7 @@ function is_verified($username) {
     return false;
 }
 
+    //For log in
 function exist_user($user, $pass) {
     $sql = "SELECT * FROM users WHERE username='$user'";
     
@@ -63,29 +95,42 @@ function exist_user($user, $pass) {
     } else return null;
 }
 
-// Admin side
+function check_username($username) {
+    $sql = "SELECT username FROM users WHERE username = '$username'";
 
-function check_username($user_id, $username, $conn) {
-    $sql = "SELECT user_id, username FROM users";
-
-    $result = query($sql, $conn);
-
-    if(empty($username)) {
-        $_SESSION['error']['emptyUsername'] = true;
-        header("Location: ../views/admin/view_admin_edit_user.php?id=$user_id");
-        die;
-    }
-
-    while($data = mysqli_fetch_assoc($result)) {
-        $data_check[] = $data;
-    }
-    foreach($data_check as $row){
-        if($username == $row['username']) {
-            if($row['user_id'] == $user_id) continue;
-            
-            $_SESSION['error']['errorUsername'] = true;
-            header("Location: ../views/admin/view_admin_edit_user.php?id=$user_id");
-            die;
-        }
-    }
+    $result = $GLOBALS['mysqli']->query($sql);
+    
+    if(mysqli_num_rows($result) == 1) return true;
+    
+    return false;
 }
+
+function insert_user(){
+    
+}
+
+// Admin side
+// function check_username($user_id, $username, $conn) {
+//     $sql = "SELECT user_id, username FROM users";
+
+//     $result = query($sql, $conn);
+
+//     if(empty($username)) {
+//         $_SESSION['error']['emptyUsername'] = true;
+//         header("Location: ../views/admin/view_admin_edit_user.php?id=$user_id");
+//         die;
+//     }
+
+//     while($data = mysqli_fetch_assoc($result)) {
+//         $data_check[] = $data;
+//     }
+//     foreach($data_check as $row){
+//         if($username == $row['username']) {
+//             if($row['user_id'] == $user_id) continue;
+            
+//             $_SESSION['error']['errorUsername'] = true;
+//             header("Location: ../views/admin/view_admin_edit_user.php?id=$user_id");
+//             die;
+//         }
+//     }
+// }
